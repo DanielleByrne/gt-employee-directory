90a7b0bbbfb0161b52f4ab87a808be05d237af8d
Danielle Byrne <DanielleByrne421@gmail.com>
2020-08-02 17:19:33 -0400

    working on getting the axios call to return only necessary data

---
+++ src/containers/employees.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+class employees extends Component {
+
+    state = {
+        employee : []
+    }
+
+    componentDidMount(){
+        axios 
+        .get("https://randomuser.me/api/")
+        .then((response)=>{
+            console.log(response.data)
+            this.setState({
+                employee: response.data.message
+            })
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>the employees page </h1>
+            </div>
+        );
+    }
+}
+
+export default employees;
